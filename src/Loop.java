4c16a1fe4d0cb3afc7afb2b8b4ade96bdfcf14e4
mohankumaresan <sendtome9916@gmail.com>
2024-06-21 21:54:05 +0530

    Add files via upload
    firstcommit

---
+++ src/Loop.java
@@ -0,0 +1,21 @@
+public class Loop {
+
+    public static void main(String[] args) {
+        for (int i = 9; i <= 37; i += 6) {
+
+            System.out.println("i value is" + i);
+        }
+
+        for (int i = 67; i >= 12; i -= 4) {
+
+            System.out.println("i value is" + i);
+        }
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("i value is" + i*6);
+        }
+
+
+    }
+}
