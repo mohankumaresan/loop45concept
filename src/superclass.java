815a9644c52331454b8ed0ada44b5f6ce317f5b7
mohan <sendtome9916@gmail.com>
2024-06-26 05:38:20 +0530

    commit

---
+++ src/superclass.java
@@ -0,0 +1,24 @@
+public class superclass {
+
+    protected int add(int a,int b,int c){
+        int result = a+b+c;
+        return result;
+    }
+    public int add(int a,int b){
+        int result1 = a+b;
+        return result1;
+    }
+    public static void main (String[] args){
+        superclass obj=new superclass();
+    }
+
+}
+class childclass  extends superclass{
+
+    public static void main(String[] args )
+    {
+        childclass obj1=new childclass();
+        System.out.println(obj1.add(42,13,20));
+        System.out.println(obj1.add(42,13));
+    }
+}
